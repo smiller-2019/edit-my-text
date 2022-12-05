dd86859067775907615fffb7beecb9cd8ceb297c
Susan Miller <sjm_02@hotmail.com>
2022-12-05 21:52:17 +0000

    Clean up

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: "development",
@@ -19,7 +16,6 @@ module.exports = () => {
     },
     plugins: [
       new HtmlWebpackPlugin({
-        title: "Hot Module Reloading",
         template: "./index.html",
       }),
       // Injects our custom service worker
